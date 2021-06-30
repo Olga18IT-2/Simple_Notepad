bbaa6d9edcd4fe25fcfecb06f27f5865e1ec4696
Olga Chukova <Olga Chukova@LAPTOP-OF-OLGA>
2021-07-01 00:31:01 +0300

    Добавьте файлы проекта.

---
+++ SimpleNotepad/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SimpleNotepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьНовыйДокументToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьСуществующийДокументtxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьТекущийДокументToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Date = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.Name_File = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьНовыйДокументToolStripMenuItem,
+            this.открытьСуществующийДокументtxtToolStripMenuItem,
+            this.сохранитьТекущийДокументToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьНовыйДокументToolStripMenuItem
+            // 
+            this.создатьНовыйДокументToolStripMenuItem.Name = "создатьНовыйДокументToolStripMenuItem";
+            this.создатьНовыйДокументToolStripMenuItem.Size = new System.Drawing.Size(351, 26);
+            this.создатьНовыйДокументToolStripMenuItem.Text = "Создать новый документ";
+            this.создатьНовыйДокументToolStripMenuItem.Click += new System.EventHandler(this.создатьНовыйДокументToolStripMenuItem_Click);
+            // 
+            // открытьСуществующийДокументtxtToolStripMenuItem
+            // 
+            this.открытьСуществующийДокументtxtToolStripMenuItem.Name = "открытьСуществующийДокументtxtToolStripMenuItem";
+            this.открытьСуществующийДокументtxtToolStripMenuItem.Size = new System.Drawing.Size(351, 26);
+            this.открытьСуществующийДокументtxtToolStripMenuItem.Text = "Открыть существующий документ .txt";
+            this.открытьСуществующийДокументtxtToolStripMenuItem.Click += new System.EventHandler(this.открытьСуществующийДокументtxtToolStripMenuItem_Click);
+            // 
+            // сохранитьТекущийДокументToolStripMenuItem
+            // 
+            this.сохранитьТекущийДокументToolStripMenuItem.Name = "сохранитьТекущийДокументToolStripMenuItem";
+            this.сохранитьТекущийДокументToolStripMenuItem.Size = new System.Drawing.Size(351, 26);
+            this.сохранитьТекущийДокументToolStripMenuItem.Text = "Сохранить текущий документ";
+            this.сохранитьТекущийДокументToolStripMenuItem.Click += new System.EventHandler(this.сохранитьТекущийДокументToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(882, 797);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Date,
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.Name_File});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(882, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Date
+            // 
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(41, 22);
+            this.Date.Text = "Data";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(61, 22);
+            this.toolStripLabel2.Text = "             ";
+            // 
+            // Name_File
+            // 
+            this.Name_File.Name = "Name_File";
+            this.Name_File.Size = new System.Drawing.Size(73, 22);
+            this.Name_File.Text = "name_file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 853);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Текстовый редактор Simple Notepad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьНовыйДокументToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьСуществующийДокументtxtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьТекущийДокументToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel Date;
+        private System.Windows.Forms.ToolStripLabel Name_File;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+    }
+}
+
---
+++ SimpleNotepad/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleNotepad
+{
+    public partial class Form1 : Form
+    {
+        private string file_text; // содержимое документа
+        private string file_name; // наименование документа
+        
+        public Form1()
+        {   
+            InitializeComponent();
+            file_name = "Новый документ";
+            this.Text = "Текстовый редактор Simple Notepad";
+            file_text = "";
+            Name_File.Text = file_name;
+            Date.Text = Convert.ToString(DateTime.Today.ToLongDateString());
+        }
+
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            file_text = richTextBox1.Text;
+            
+            string stroka = file_text;
+            stroka = stroka.Replace(" ", string.Empty);    
+            stroka = stroka.Replace("\n", string.Empty);
+
+            if (stroka.Length == 0)
+            {
+                this.Close();
+            }
+            else
+            {
+                string answer = Save_this_text(file_name), save;
+                if (answer == "Yes")
+                {
+                    save = Save_file(file_text);
+                    if (save == "Ok")
+                    {
+                        MessageBox.Show(" Файл успешно сохранён !!! ");
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    if (answer == "No")
+                    {
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void открытьСуществующийДокументtxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ( openFileDialog1.ShowDialog() == DialogResult.OK &&
+                openFileDialog1.FileName.Length > 0 )
+            {   
+                file_name = openFileDialog1.FileName;
+                this.Text = "Текстовый редактор Simple Notepad"; 
+                Name_File.Text = file_name;
+                file_text = System.IO.File.ReadAllText(file_name);
+                richTextBox1.Text = file_text;
+                MessageBox.Show("   Файл " + file_name + " успешно открыт ! ");
+            }
+        }
+
+        private void создатьНовыйДокументToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            file_text = richTextBox1.Text;
+            string stroka = file_text;
+            stroka = stroka.Replace(" ", string.Empty);
+            stroka = stroka.Replace("\n", string.Empty);
+
+            if (stroka.Length == 0) richTextBox1.Clear();
+            else
+            {
+                string answer = Save_this_text(file_name);
+                if (answer == "Yes")
+                { 
+                    if (Save_file(file_text) == "Ok") 
+                    {
+                        Main_info_of_NewDocument(); 
+                    } 
+                }
+                else
+                {
+                    if (answer == "No") 
+                    { 
+                        Main_info_of_NewDocument(); 
+                    }
+                }
+            }  
+        }
+
+        private void сохранитьТекущийДокументToolStripMenuItem_Click(object sender, EventArgs e)
+        { 
+            string save = Save_file(richTextBox1.Text); 
+        }
+
+        public void Main_info_of_NewDocument() 
+        {
+            richTextBox1.Clear();
+            file_name = "Новый документ";
+            this.Text = "Текстовый редактор Simple Notepad"; Name_File.Text = file_name;
+            file_text = "";
+            Name_File.Text = file_name;
+        }
+
+        public string Save_this_text (string file_name) 
+        {
+            DialogResult result = MessageBox.Show
+                (" Сохранить изменения в " + file_name + " ??? ", 
+                " SimpleNotepad ", MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (result == DialogResult.Yes) 
+                return "Yes";
+            else
+            {
+                if (result == DialogResult.No) 
+                    return "No";
+                else 
+                { 
+                    if (result == DialogResult.Cancel) 
+                        return "Cancel"; 
+                    else 
+                        return ""; }
+            }
+        }
+        public string Save_file(string file_text) 
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.Cancel) 
+                return "Cancel";
+            else
+            {
+                if (result == DialogResult.OK && saveFileDialog1.FileName.Length > 0)
+                {
+                    string file_name = saveFileDialog1.FileName;
+                    System.IO.File.WriteAllText(file_name, file_text);
+                    this.Text = "Текстовый редактор Simple Notepad"; 
+                    Name_File.Text = file_name;
+                    MessageBox.Show(" Файл " + file_name + "успешно сохранён !!! ");
+                }
+                return "Ok";
+            }
+        }
+
+    }
+}
